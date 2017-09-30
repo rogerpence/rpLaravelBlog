d6e4aba21ee9bb3f3364bf0529abd6902bfc4375
Roger Pence <roger.pence@gmail.com>
2017-09-30 15:14:28 -0500

    convert css to BS 4 beta

--- resources/views/masters/main.blade.php
+++ resources/views/masters/main.blade.php
@@ -9,88 +9,127 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('assets/css/foundation.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+        <script src="https://use.fontawesome.com/ca38661c31.js"></script>           
     </head>
 
     <body>
-        <div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
-            <button class="menu-icon" type="button" data-toggle="example-menu"></button>
-            <div class="title-bar-title">Menu</div>
-        </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <!-- <a class="navbar-brand" href="#">rogerpence.com</a> -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div class="top-bar" id="example-menu">
-            <div class="top-bar-left">
-                <ul class="dropdown menu" data-dropdown-menu>
-                    <li class="menu-text">Site Title</li>
-                    <li><a href="#0">About</a></li>
-                    <li><a href="#0">Contact</a></li>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">About <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
                 </ul>
-            </div>
-            <div class="top-bar-right">
-                <ul class="menu">
-                    
-                    <!-- Search commented out for now.
-                        <li><input type="search" placeholder="Search"></li>
-                        <li><button type="button" class="button">Search</button></li> 
-                    -->
+                <!-- <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form> -->
 
-                    @guest                        
-                        <li><a href="{{ route('login') }}">Login</a></li>                
-                        <li><a href="{{ route('Register') }}">Register</a></li>
-                    @else                        
-                        <ul class="dropdown menu" data-dropdown-menu>
-                            <li>
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                            {{ Auth::user()->name }} <span class="caret"></span>
+                <!-- <ul class="nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" 
+                            aria-haspopup="true" aria-expanded="false">Roger Pence</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Logout</a>
+                            <a class="dropdown-item" href="#">Profile</a>
+                        </div>
+                    </li>
+                </ul> -->
+
+                <ul class="nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" 
+                            aria-haspopup="true" aria-expanded="false">
+                            @guest 
+                                Account
+                            @else 
+                                {{ Auth::user()->name }}
+                            @endguest                         
+                        </a>
+                        <div class="dropdown-menu">
+                            @guest
+                                <a class="dropdown-item" href="{{ route('login') }}">Login</a>
+                                <a class="dropdown-item" href="{{ route('register') }}">Register</a>
+                            @else 
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                    Logout
                                 </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>                                                                
+                            @endguest
+                        </div>
+                    </li>
+                </ul>                
+            </div>
+        </nav>
 
-                                <ul class="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>                                    
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-
-
-                    @endguest                        
-                </ul>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-5">rogerpence.com</h1>
+                <p class="lead">A nerd's blog</p>
             </div>
         </div>
 
+        <div class="container">
+            <div class="row">
+                <div class="col-8">
+                    <div class="post">
+                        <h2>Launch VirtualBox VM with a .desktop file</h2>
+                        <div class="post-date-line">
+                            <i class="fa fa-calendar" title="Date posted"></i> 3 days ago&nbsp;&nbsp;&nbsp;
+                            <a href="#">Leave a comment</a>
+                        </div>
+                        <p>
+                            I’m using a VirutalBox VM on a PHP project The VM is a desktop version of Ubuntu 17.04, sometimes used with one monitor and sometimes used with two. VB works great in this environment but its multiple monitor capabilities are a little prickly. Launching the VM with one monitor when the settings are set for… </p>
+                        <div>
+                            <button type="button" class="btn btn-sm btn-primary">Read more</button>&nbsp;&nbsp;&nbsp;
+                            <i class="fa fa-pencil-square-o" title="Author"></i> rp&nbsp;&nbsp;&nbsp;
+                            <i class="fa fa-tags" title="Tags"></i> <a href="#">db</a>&nbsp;&nbsp;&nbsp;
+                            <a title="Comments" href="#"><i class="fa fa-comments" title="Comments"></i>&nbsp;3</a>
+                        </div>
+                        <hr>
+                    </div>
+                    <div class="post">
+                        <h2>How to reset MariaDB’s root user password</h2>
+                        <div class="post-date-line">
+                            <i class="fa fa-calendar" title="Date posted"></i> 3 days ago&nbsp;&nbsp;&nbsp;
+                            <a href="#">Leave a comment</a>
+                        </div>
+                        <p>
+                            It’s really annoying to forget what your MariaDB’s root account password is. However, with just a little commandline effort, it’s an easy challenge to resolve. I learned most of these instructions with this Digial Ocean article. It’s a great article but
+                            it instructions for ending the MariaDB mysqld process didn’t work for me on Ubuntu… 
+                        </p>
+                        <div>
+                            <button type="button" class="btn btn-sm btn-primary">Read more</button>&nbsp;&nbsp;&nbsp;
+                            <i class="fa fa-pencil-square-o" title="Author"></i> rp&nbsp;&nbsp;&nbsp;
+                            <i class="fa fa-tags" title="Tags"></i> <a href="#">db</a>&nbsp;&nbsp;&nbsp;
+                            <a title="Comments" href="#"><i class="fa fa-comments" title="Comments"></i>&nbsp;3</a>
+                        </div>
+                        <hr>
+                    <div class="col">
+                    <!-- Sidebar here. -->
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        <div>
-            @yield('content')
+                </div>
+            </div>
         </div>
 
         <!-- Scripts -->
         <script src="{{ asset('assets/js/vendor/jquery.js') }}"></script>
-        <script src="{{ asset('assets/js/vendor/what-input.js') }}"></script>
-        <script src="{{ asset('assets/js/vendor/foundation.js') }}"></script>
+        <script src="{{ asset('assets/js/vendor/popper.js') }}"></script>
+        <script src="{{ asset('assets/js/vendor/bootstrap.min.js') }}"></script>
         <script src="{{ asset('assets/js/app.js') }}"></script>
     </body>
 </html>
