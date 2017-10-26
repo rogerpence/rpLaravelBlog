4e22280229d74ea3bd81efe129a7308eb141b217
Roger Pence <roger.pence@gmail.com>
2017-10-25 22:04:42 -0500

    add tags model and start tags api

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $tags = ['javascript','laravel','php', 
+                  'database', 'mysql', 'python',
+                  'devops', 'nginx', 'css'];  
+                     
+        foreach($tags as $tag) {
+            App\Tag::create([
+                'name' => $tag
+            ]);
+        }                    
+    }  
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,14 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('tags', function () {
+//    $tags = \App\Tag::orderBy('name')->pluck('name');
+    $find = 'l';
+    //$tags = \App\Tag::orderBy('name')->where('name', 'like', $find . '%')->pluck('name');
+    $tags = \App\Tag::orderBy('name')->where('name', '>=', $find)->pluck('name');
+    return $tags;
 });
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
