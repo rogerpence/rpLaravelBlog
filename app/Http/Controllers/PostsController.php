8e49626f2559d5a50376a4fa04fb84179adbdb3f
Roger Pence <roger.pence@gmail.com>
2017-10-28 12:11:45 -0500

    fix PostController reference

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Http\Request;
-use App\Classes\Repository;
+use App\Classes\PostsRepository;
 use App\Comment;
 
 class PostsController extends Controller
--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,6 @@
 #code .
 # ff 127.0.0.1:5000	
-google-chrome 127.0.0.1:5000
+#google-chrome 127.0.0.1:5000
+#sleep 4
 php artisan serve --port=5000
 
