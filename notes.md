c6fbcf841a9a26a464ec6e47994e5aac7f6aa223
roger pence <roger.pence@gmail.com>
2017-11-19 13:58:34 -0600

    diddle with regex for prettyprint

--- notes.md
+++ notes.md
@@ -13,3 +13,17 @@ $post->tags->pluck('name')
 
 
 export XDEBUG_CONFIG="idekey=VSCODE"
+
+$re = '/\{\{\s*lang=(\s*\w*:.*)\}\}/';
+$str = 'abc de {{ lang=js:language:rogerpence1 }} asdf
+
+abc de {{ lang=js:language:rogerpence2 }} asdf
+
+abc de {{ lang=js:language:rogerpence3 }} asdf
+
+';
+
+preg_match_all($re, $str, $matches, PREG_SET_ORDER, 0);
+
+// Print the entire match result
+var_dump($matches);
--- test.php
+++ test.php
@@ -1,3 +1,28 @@
 <?php
+$re = '/\{\{\s*lang=(\s*\w*:.*)\}\}/';
+$str = 'abc de {{ lang=js:language:rogerpence1 }} asdf
 
-exec('python getCommit.py');
+abc de {{lang=js:language:rogerpence2}} asdf
+
+abc de {{ lang=js:language:rogerpence3 }} asdf
+
+';
+
+preg_match_all($re, $str, $matches, PREG_SET_ORDER, 0);
+
+//var_dump($matches);
+
+foreach ($matches as $match) {
+    //var_dump($match);
+    //var_dump(sizeof($match));
+    print_r($match[0] . "\n");
+    print_r($match[1] . "\n");
+    
+    // foreach ($match as $token) {
+    //     //var_dump($token);
+    //     var_dump(sizeof($token));
+    //     //print_r(size($token));
+    //     //print($token . "\n");
+    // }
+    //print_r($match);
+}
