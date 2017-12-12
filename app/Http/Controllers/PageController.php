7dfcdcc61d271b1f401db906af4efd319bb1a383
roger pence <roger.pence@gmail.com>
2017-12-11 23:27:21 -0600

    remove commented out code

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,12 +6,11 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    // public function show(\App\Post $post) 
     public function show($slug) 
     {
+        // Fetch all slugs for posts with 'page' status.
         $pages = \App\Post::where('status', '=', 2)->pluck('slug')->toArray();
 
-        //$pages = ['about', 'contact'];
         if (in_array($slug, $pages)) {        
             $post = \App\Post::where('slug', '=', $slug )->first();
             return view('posts.show-page')->with('post', $post)->with('post_id', $post->id);
@@ -19,16 +18,5 @@ class PageController extends Controller
         else {
             abort(404);                    
         }
-
-
-        // $post = \App\Post::where('slug', '=', $slug )->first();
-        // $comments = $post->hasMany(Comment::class)->where('comment_id',0)->where('approved', true)->get();
-        // $tag_array = $post->tags->pluck('name')->toArray();
-        // $taglist = implode(",", $tag_array);
-
-        // return view('posts.show-new')->with('post', $post)->
-        //                            with('comments', $comments)->
-        //                            with('slug', $slug)->
-        //                            with('post_id', $post->id);
     }        
 }
