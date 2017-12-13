849c381417e6e0b8ec2c76994d7a91fcadfffdde
roger pence <roger.pence@gmail.com>
2017-12-12 22:56:09 -0600

    tweak favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -8,7 +8,7 @@
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('assets/js/vendor/bootstrap.css') }}">
 
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">    
+    <link rel="shortcut icon" href="favicon.ico?" type="image/x-icon">    
 
     <!-- MDB css -->
     <!-- <link rel="stylesheet" href="{{ asset('assets/mdb/css/mdb.min.css') }}"> -->
