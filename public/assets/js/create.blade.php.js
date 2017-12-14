8a8f2bb0763245c2ea12908b802c469f431a7d6f
roger pence <roger.pence@gmail.com>
2017-12-14 00:03:52 -0600

    major refactor of create.blade.php.js

--- public/assets/js/create.blade.php.js
+++ public/assets/js/create.blade.php.js
@@ -1,89 +1,143 @@
-let valuesOriginal;
-let valuesCurrent;
+var rp = rp || {};
 
-let collectInputs = () => {
-    inputs = document.querySelectorAll('.save');
+// rp.globals = (function() {
+//     let valuesOriginal;
+//     let valuesCurrent;
 
-    let values = [];
-    for (var i = 0; i < inputs.length; i++) {
-        values.push({
-            'id': inputs[i].id,
-            'value': inputs[i].value,
-            'newValue' : '',
-            'name': inputs[i].name,
-            'changed': false
-        });
-    }
-    return values;
+//     return {
+//         valuesOriginal: valuesOriginal,
+//         valuesCurrent: valuesCurrent
+//     };
+// })();
+
+rp.globals = {
+    valuesOriginal: null,
+    valuesCurrent: null
 }
 
+rp.abstractMarkdownEditor = (function () {
+    function instance(id) {
+        var editor = new SimpleMDE({
+            element: document.getElementById(id),
+            autofocus: false,
+            indentWithTabs: true,
+            tabSize: 4,
+            shortcuts: {
+                "toggleFullScreen": "F8"
+            }
+            //toolbar: true    
+        });
 
-/*
-Insert text at beginning of line and optionally select selectText
-*/
-let currentMDEditor = '';
+        return editor;
+    }
+    return {
+        instance: instance
+    };       
+})();            
 
-/*
-{
-    'key': 'O',
-        'text': '{{prettify=js:linenums}}',
-            'select' : 'js'
-},    
-*/
+rp.bodyMarkdownEditor = (function () {
+    function instance(id) {
+        var editor = new SimpleMDE({
+            element: document.getElementById(id),
+            autosave: {
+                enabled: false,
+                uniqueId: 'bodyContent',
+                delay: 1000
+            },
+            indentWithTabs: false,
+            autofocus: false,
+            tabSize: 4,
+            shortcuts: {
+                "toggleFullScreen": "F8"
+            }
+        });
 
-let macros = [
-    {
-        'key': 'Ctrl-L',
-        'text': '<!--prettify lang=js linenums=true-->',
-        'select':'js'
-    },
-    {
-        'key': 'Ctrl-M',
-        'text': '<small>caption</small>',
-        'select': 'caption'
-    }    
-];
+        setCustomKeys(editor);
+    }        
 
-/*
-<div class="thumbnail pad-box-lg align-left" >
-    <img src = "/media/images/avr-does-lots-of-things.png" >
-    <div class = "text-center" >
-       <small>AVR empowers your RPG programmers to do</small>
-    </div>       
-</div>
-*/
+    function setCustomKeys(editor) {
+        editor.codemirror.setOption("extraKeys", {
+            'Ctrl-End': function (cm) {
+                cm.execCommand('goLineRight');
+            },
+            'Ctrl-Home': function (cm) {
+                cm.execCommand('goLineLeftSmart');
+            },
+            'Ctrl-L': function (cm) {
+                rp.pureFunctions.insertTextAtCurrentLine(cm, '<!--prettify lang=js linenums=true-->', 'js');
+            },
+            'Ctrl-M': function (cm) {
+                rp.pureFunctions.insertTextAtCurrentLine(cm, '<small>caption</small>', 'caption');
+            }
+        });
+    }                        
 
+    // function setEventHandlers(editor) {
+    //     simplemdeBody.codemirror.on('focus', () => {
+    //         currentMDEditor = 'body';
+    //     });
 
-(function() {
-    document.getElementById('title').addEventListener('input', function () {
-        var slug = document.getElementById('slug');
-        var slugText = this.value.replace(/(\s+)/g, '-').toLowerCase();
-        slugText = slugText.replace(/(\.)/g, '').toLowerCase();
-        slug.value = slugText;
-    });
+    //     simplemdeBody.codemirror.on('blur', () => {
+    //         currentMDEditor = '';
+    //     });
+    // }        
+
+    return {
+        instance: instance
+    };       
+})();    
+
+rp.simpleControls = (function () {
+    function addDatePicker() {
+        const dp = document.getElementById('date-to-publish');
+        const fp = flatpickr(dp, {});
+    }        
+
+    function addTypingTracker() {
+        const MAX_SEO_TITLE_LENGTH = 70;
+        const MAX_SEO_DESC_LENGTH = 160;
+        new rp.core.TypingTracker('title', MAX_SEO_TITLE_LENGTH)
+        new rp.core.TypingTracker('seo_description', MAX_SEO_DESC_LENGTH)
+    }
+    
+    function initializeBootStrapComponents() {
+        $('[data-toggle="popover"]').popover()
+    }        
+
+    function configure() {
+        addDatePicker();
+        addTypingTracker();
+        initializeBootStrapComponents();
+    }
+
+    return {
+        configure: configure
+    };
 })();
 
-// window.onbeforeunload = function(e) {
-//     var message = 'Are you sure you want abandon unsaved changes on this page?';
-//     valuesCurrent = collectInputs();
-//     let cancelUnload = false;
-//     for (let i = 0; i < valuesCurrent.length; i++) {
-//         if (valuesOriginal[i].value !== valuesCurrent[i].value) {
-//             valuesOriginal[i].changed = true;
-//             valuesOriginal[i].newValue = valuesCurrent[i].value;
-//             cancelUnload = true;
-//         }
-//     }
-//     if (cancelUnload) {
-//         var e = e || window.event;
-//         if (e) {
-//             e.returnValue = message;
-//         }
-//         return message;
-//     }                
-// };
+// rp.simpleControls = (function () {
 
-let documentReady = () => {
+//     return {
+
+//     };        
+// })();
+
+rp.pureFunctions = (function () {
+    function collectInputs() {
+        inputs = document.querySelectorAll('.save');
+
+        let values = [];
+        for (var i = 0; i < inputs.length; i++) {
+            values.push({
+                'id': inputs[i].id,
+                'value': inputs[i].value,
+                'newValue': '',
+                'name': inputs[i].name,
+                'changed': false
+            });
+        }
+        return values;
+    }
 
     function insertTextAtCurrentLine(cm, text, selectText) {
         let cursorInfo = cm.getCursor('from');
@@ -104,19 +158,6 @@ let documentReady = () => {
             cm.setCursor(cursorInfo);
         }
     }
-
-    // Add hot keys to add/change page.
-    document.addEventListener('keydown', (e) => {
-        const S_Key = 83;
-       
-        // Page level keys.
-        if (e.ctrlKey && e.keyCode == S_Key) {
-            e.stopPropagation();
-            e.preventDefault();
-            document.getElementById('post-content-form').submit();
-            return false;
-        }   
-    }, true);
 
     function post(path, params, method) {
         method = method || "post"; // Set method to post by default if not specified.
@@ -142,200 +183,183 @@ let documentReady = () => {
         form.submit();
     }
 
-    document.getElementById('title').addEventListener('input', function () {
-        var slug = document.getElementById('slug');
-        var slugText = this.value.replace(/(\s+)/g, '-').toLowerCase();
-        slugText = slugText.replace(/(\.)/g, '').toLowerCase();
-        slug.value = slugText;
-    });
-
-    var simplemdeAbstract = new SimpleMDE({
-        element: document.getElementById("abstract"),
-        autofocus: false,
-        indentWithTabs: true,
-        tabSize: 4,        
-        shortcuts: {
-            "toggleFullScreen": "F8"
-        }        
-        //toolbar: true
-    });
-
-    var simplemdeBody = new SimpleMDE({
-        element: document.getElementById("body"),
-        autosave: {
-            enabled: false,
-            uniqueId: 'bodyContent',
-            delay: 1000
-        },            
-        indentWithTabs: false,
-        autofocus: false,
-        tabSize: 4,
-        shortcuts: {
-            "toggleFullScreen": "F8" 
-        }
-    });
-
-    simplemdeBody.codemirror.setOption("extraKeys", {
-        'Ctrl-End': function (cm) {
-            cm.execCommand('goLineRight');
-        },
-        'Ctrl-Home': function(cm) {
-            cm.execCommand('goLineLeftSmart');
-        },
-        'Ctrl-L': function(cm) {
-            insertTextAtCurrentLine(cm, '<!--prettify lang=js linenums=true-->', 'js');
-        },
-        'Ctrl-M': function(cm) {
-            insertTextAtCurrentLine(cm, '<small>caption</small>', 'caption');
-        }
-    });        
-
-    simplemdeBody.codemirror.on('focus', () => {
-        currentMDEditor = 'body';
-    });
-
-    simplemdeBody.codemirror.on('blur', () => {
-        currentMDEditor = '';
-    });
-
-    const MAX_SEO_TITLE_LENGTH = 70;
-    const MAX_SEO_DESC_LENGTH = 160;
-
-    new rp.core.TypingTracker('title', MAX_SEO_TITLE_LENGTH)
-    new rp.core.TypingTracker('seo_description', MAX_SEO_DESC_LENGTH)
-
-    $('[data-toggle="popover"]').popover()
-
-    const tagChiefOptions = {
-        editableTags: {
-            tagTextInputId: 'tag-text-input', // default is 'tag-text-input'
-            inputTagIdForServer: 'tag-list-for-server', // default is 'tag-list-for-server'
-            onTagAddedHandler: (tag) => {
-                rp.tagChiefDataListProvider.appendTagIfAdhocTag(tag);
-                rp.tagChiefDataListProvider.clearAndFocusTagInputElement();
-            },
-            onTagRemovedHandler: (tag) => {
-                rp.tagChiefDataListProvider.removeIfAdhocTag(tag);
-            },
-            onDupeDetected: (tag) => {
-                rp.tagChiefDataListProvider.hideDatalistDropDown();
-            }
-        }
+    return {
+        collectInputs: collectInputs,
+        insertTextAtCurrentLine: insertTextAtCurrentLine,
+        post: post
     };
+})();
 
-    rp.tagchief.setOptions(tagChiefOptions);
-
-    let providerOptions = {
-        datalistId: 'all-tags',
-        tagTextInputId: 'tag-text-input',
-        inputHandlerFn: (e, obj) => {
-            // Handles 'input' event from tagTextInputId element. The e argument
-            // is the event object passed to the 'input' handler and the 'obj'
-            // is the element identified by 'tagTextInputId.' That option value
-            // can't be directly referenced here because it is out of scope.  
-            let tagText = obj.value;
-            let isDupe = rp.tagchief.isDuplicate(tagText);
-
-            if (rp.tagChiefDataListProvider.isTagInTagList(tagText) && !isDupe) {
-                rp.tagchief.addTag(tagText);
-                obj.value = '';
-                obj.focus();
-            }
-
-            if (isDupe) {
-                obj.value = '';
-                obj.focus();
-            }
-        },
-        url: '/api/tags'
-        //list: ['a','b','c']       
-    };
-
-    rp.tagChiefDataListProvider.initialize(providerOptions);
-
-    const dp = document.getElementById('date-to-publish');
-    const fp = flatpickr(dp, {});
-
-    const isDeletable = document.getElementById('delete-post');
-    if (isDeletable) {
-        // instanciate new modal
-        var modal = new tingle.modal({
-            footer: true,
-            stickyFooter: false,
-            closeMethods: ['escape'],
-            closeLabel: "Close",
-            cssClass: ['custom-class-1', 'custom-class-2'],
-            onOpen: function () {
-                //console.log('modal open');
-            },
-            onClose: function () {
-                //console.log('modal closed');
-            },
-            beforeClose: function () {
-                // here's goes some logic
-                // e.g. save content before closing the modal
-                return true; // close the modal
-                //return false; // nothing happens
-            }
+rp.eventHandlers = (function () {
+    function add() {
+        document.getElementById('title').addEventListener('input', function () {
+            var slug = document.getElementById('slug');
+            var slugText = this.value.replace(/(\s+)/g, '-').toLowerCase();
+            slugText = slugText.replace(/(\.)/g, '').toLowerCase();
+            slug.value = slugText;
         });
 
-        // set content
-        postTitle = document.getElementById('title').value;
-        modal.setContent('<h4>Are you sure you want to delete this post?</h4>' + 
-        '<h4>' + postTitle + '</h4>');
+        // Global page hot keys.
+        document.addEventListener('keydown', (e) => {
+            const S_Key = 83;
 
-        // add a button
-        modal.addFooterBtn('No', 'tingle-btn tingle-btn--primary', function () {
-            modal.close();
-        });
-        
-        // add another button
-        modal.addFooterBtn('Yes', 'tingle-btn tingle-btn--danger', function () {
-            // here goes some logic
-            let parms = {};        
-            let el = document.querySelector('input[name="_token"]');
-            parms._token = el.value;
-            parms._method = 'delete';
+            if (e.ctrlKey && e.keyCode == S_Key) {
+                e.stopPropagation();
+                e.preventDefault();
+                document.getElementById('post-content-form').submit();
+                return false;
+            }
+        }, true);
 
-            let post_id = document.getElementById('postid').value;
-
-            post(`/post/${post_id}`, parms);        
-        });    
-
-        document.getElementById('delete-post').addEventListener('click', (e) => {
-            modal.open()
-        });    
+        window.onbeforeunload = function (e) {
+            //     var message = 'Are you sure you want abandon unsaved changes on this page?';
+            //     valuesCurrent = collectInputs();
+            //     let cancelUnload = false;
+            //     for (let i = 0; i < valuesCurrent.length; i++) {
+            //         if (valuesOriginal[i].value !== valuesCurrent[i].value) {
+            //             valuesOriginal[i].changed = true;
+            //             valuesOriginal[i].newValue = valuesCurrent[i].value;
+            //             cancelUnload = true;
+            //         }
+            //     }
+            //     if (cancelUnload) {
+            //         var e = e || window.event;
+            //         if (e) {
+            //             e.returnValue = message;
+            //         }
+            //         return message;
+            //     }                
+        };
     }        
 
-    valuesOriginal = collectInputs();
+    return {
+        add: add
+    };        
+})();
+
+rp.tags = (function () {
+    function configure() {
+        const tagChiefOptions = {
+            editableTags: {
+                tagTextInputId: 'tag-text-input', // default is 'tag-text-input'
+                inputTagIdForServer: 'tag-list-for-server', // default is 'tag-list-for-server'
+                onTagAddedHandler: (tag) => {
+                    rp.tagChiefDataListProvider.appendTagIfAdhocTag(tag);
+                    rp.tagChiefDataListProvider.clearAndFocusTagInputElement();
+                },
+                onTagRemovedHandler: (tag) => {
+                    rp.tagChiefDataListProvider.removeIfAdhocTag(tag);
+                },
+                onDupeDetected: (tag) => {
+                    rp.tagChiefDataListProvider.hideDatalistDropDown();
+                }
+            }
+        };
+
+        rp.tagchief.setOptions(tagChiefOptions);
+
+        let providerOptions = {
+            datalistId: 'all-tags',
+            tagTextInputId: 'tag-text-input',
+            inputHandlerFn: (e, obj) => {
+                // Handles 'input' event from tagTextInputId element. The e argument
+                // is the event object passed to the 'input' handler and the 'obj'
+                // is the element identified by 'tagTextInputId.' That option value
+                // can't be directly referenced here because it is out of scope.  
+                let tagText = obj.value;
+                let isDupe = rp.tagchief.isDuplicate(tagText);
+
+                if (rp.tagChiefDataListProvider.isTagInTagList(tagText) && !isDupe) {
+                    rp.tagchief.addTag(tagText);
+                    obj.value = '';
+                    obj.focus();
+                }
+
+                if (isDupe) {
+                    obj.value = '';
+                    obj.focus();
+                }
+            },
+            url: '/api/tags'
+            //list: ['a','b','c']       
+        };
+
+        rp.tagChiefDataListProvider.initialize(providerOptions);
+    }        
+
+    return {
+        configure: configure
+    };
+
+})();        
+
+rp.deletePostModal = (function() {
+    function configure() {        
+        const isDeletable = document.getElementById('delete-post');
+        if (isDeletable) {
+            // instanciate new modal
+            var modal = new tingle.modal({
+                footer: true,
+                stickyFooter: false,
+                closeMethods: ['escape'],
+                closeLabel: "Close",
+                cssClass: ['custom-class-1', 'custom-class-2'],
+                onOpen: function () {
+                    //console.log('modal open');
+                },
+                onClose: function () {
+                    //console.log('modal closed');
+                },
+                beforeClose: function () {
+                    // here's goes some logic
+                    // e.g. save content before closing the modal
+                    return true; // close the modal
+                    //return false; // nothing happens
+                }
+            });
+
+            // Set modal content.
+            postTitle = document.getElementById('title').value;
+            modal.setContent('<h4>Are you sure you want to delete this post?</h4>' +
+                '<h4>' + postTitle + '</h4>');
+
+            modal.addFooterBtn('No', 'tingle-btn tingle-btn--primary', function () {
+                modal.close();
+            });
+
+            modal.addFooterBtn('Yes', 'tingle-btn tingle-btn--danger', function () {
+                let parms = {};
+                let el = document.querySelector('input[name="_token"]');
+                parms._token = el.value;
+                parms._method = 'delete';
+
+                let post_id = document.getElementById('postid').value;
+
+                rp.pureFunctions.post(`/post/${post_id}`, parms);
+            });
+
+            document.getElementById('delete-post').addEventListener('click', (e) => {
+                modal.open()
+            });
+        }        
+    }        
+
+    return {
+        configure: configure
+    }
+})();
+
+let documentReady = () => {
+    rp.bodyMarkdownEditor.instance('body');
+    rp.abstractMarkdownEditor.instance('abstract');
+    rp.simpleControls.configure();
+    rp.eventHandlers.add();
+    rp.tags.configure();
+    rp.deletePostModal.configure();
+    
+    rp.globals.valuesOriginal = rp.pureFunctions.collectInputs();
+
+    var x = rp.globals.valuesOriginal;
 };
 
 rp.core.documentReady(documentReady);
-
-
-
-
-    // $( "#tag-text-input" ).autocomplete( {
-    //     source : function(req, add) {
-    //         // req.term = value entered in text box.
-    //         let url = '/api/tags?startswith=' + req.term;
-    //         let promise = $.getJSON(url)
-    //         promise.done(function(json) {
-    //             add(json);                
-    //         });
-    //         promise.fail(function(jqxhr,textStatus,error) {    
-    //            let x = 'x';                     
-    //         });
-    //     },    
-    //     select : function(e, ui) {
-    //         document.getElementById('tag-text-input').value = '';
-    //         return false;
-
-    //     },
-    //     focus: function (event, ui) {
-    //         var menu = $(this).data("uiAutocomplete").menu.element;
-    //         focused = menu.find("li:has(a.ui-state-focus)");
-    //         console.log(focused.attr('class'));
-    //     }         
-    // });     
-
