8ca2ba42699988ae7bd55d6838a75309ccb54d44
roger pence <roger.pence@gmail.com>
2017-12-27 01:01:02 -0600

    use my new endsWith in my blade directive

--- app/Classes/MyHelpers.php
+++ app/Classes/MyHelpers.php
@@ -4,19 +4,11 @@ namespace App\Classes;
 
 class MyHelpers
 {
-    private static function endsWith($haystack, $needle)
-    {
-        $length = strlen($needle);
-
-        return $length === 0 || 
-        (substr($haystack, -$length) === $needle);
-    }    
-
     public static function pathActive($path, $token = null) {
         $token = ($token == null) ? 'active' : $token; 
         $url = request()->url();
 
-        if (\App\Classes\MyHelpers::endsWith($url, $path)) {
+        if (\StringHelper::endsWith($url, $path)) {
             return ' ' . $token . ' '; 
         }
         else {
