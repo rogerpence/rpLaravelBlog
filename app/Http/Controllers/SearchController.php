d6d68ef352daf303e5c209d304523cf11bd2136a
roger pence <roger.pence@gmail.com>
2018-01-01 01:34:43 -0600

    change tag title css

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,12 +10,33 @@ class SearchController extends Controller
 {
      public function show() 
     {
+        $special = [];
+
         $search = request('search');
         if (Auth::user()) {
             $posts = \App\Post::search($search)->get();        
+
+            // foreach ($posts as $post) {
+            //     $p = $post;
+            //     $status = $post->status;
+            //     if ($status == PostStatus::PUBLISHED Or $status == PostStatus::PAGE ) {
+            //         array_push($special, $post);
+            //     }
+            // }                
+
+            // $posts =(object) $special;
+            // $p = compact($posts);
+
         }
         else {
-            $posts = \App\Post::search($search)->where('status',PostStatus::PUBLISHED)->get();        
+            $posts = \App\Post::search($search)->where('status',PostStatus::PUBLISHED)->get();
+            
+            //)->where('status',PostStatus::PUBLISHED)->get();
+
+                    //orWhere('status',PostStatus::PAGE)->get();        
+
+
+
         }                        
 
         $view = ['search' => $search, 'mode'=> 'search' ];
