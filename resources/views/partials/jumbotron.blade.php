d1ca681c2290031bce8fa55b221c25e6d1d609f9
roger pence <roger.pence@gmail.com>
2018-02-07 18:57:07 -0600

    change home page jumbotron

--- resources/views/partials/jumbotron.blade.php
+++ resources/views/partials/jumbotron.blade.php
@@ -1,6 +1,28 @@
+
+
+
+<div class="header-container">
+   <a href="https://rogerpence.com">
+      <section class="page-header">   
+          <section class="image">
+             <img class="img-circle"
+                  src="https://rogerpence.com/storage/images/rp-small.2458154.32926.png?1"/>
+          </section>
+          <section class="text">
+             <h1 class="headline">rogerpence.com</h1>
+             <h4 class="subhead">nerdy things</h4>
+          </section>
+      </section> 
+   </a>   
+</div>
+
+
+
+<!--
 <div class="jumbotron jumbotron-fluid">
     <div>
         <a class="blog-header-link" href="/"><h1 class="display-5">rogerpence.com</h1></a>
         <p class="lead">Nerdy things</p>       
     </div>
 </div>
+-->
