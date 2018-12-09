644d2d2f08f33412b3e9f4bf35d8e3cf394da7b7
Roger Pence <roger.pence@gmail.com>
2018-12-09 15:19:29 -0600

    fix js for image markdown link

--- public/assets/js/routes.images.js
+++ public/assets/js/routes.images.js
@@ -19,100 +19,90 @@ rp.uploads = (function() {
     var getUploadedImagesCallBack = function(json) {
         showImagesList(json);
     }
-    var assignImageListButtonHandlers = () => {        
+    var assignImageListButtonHandlers = () => {
         let table = $('#datatable').DataTable();
 
         let assignButtonEvent = function(selector) {
-            let buttons = document.querySelectorAll(selector);            
-            for (let i=0; i <buttons.length; i++) {
-                buttons[i].addEventListener('click', function(e)  {
+            let buttons = document.querySelectorAll(selector);
+            for (let i = 0; i < buttons.length; i++) {
+                buttons[i].addEventListener('click', function(e) {
                     let tr = this.parentElement.parentElement;
                     var data = table.row($(tr)).data();
 
                     if (this.className == 'button-preview') {
                         rp.viewImage.showModalWindow(data);
-                    }
-                    else if (this.className == 'button-copy') { 
-                        let filename = `![${data.description}]()/storage/images/${data.name}?${data.cachebuster}`;
+                    } else if (this.className == 'button-copy') {
+                        let filename = `![${data.description}](/storage/images/${data.name}?${data.cachebuster})`;
                         rp.Lib.copyTextToClipboard(filename);
                         notifier.show('Copy to clipboard successful', 'Image URL is now available for pasting.', '', '/assets/images/survey-48.png', 4000);
-                    }
-                    else if (this.className == 'button-delete') {
+                    } else if (this.className == 'button-delete') {
                         let options = {
-                            url: `/api/images/${data.id}`,                                                    
+                            url: `/api/images/${data.id}`,
                             method: 'DELETE',
                             body: JSON.stringify(data),
                             headers: new Headers({
-                                'Content-Type': 'application/json'                    
+                                'Content-Type': 'application/json'
                             }),
                             action: rp.uploads.imageDeleted
-                        };                        
+                        };
 
                         let httpReq = new rp.ajax.HTTPRequest();
                         httpReq.submit(options);
-                    }
-                    else if (this.className == 'button-edit') {
+                    } else if (this.className == 'button-edit') {
                         rp.editImage.getSingleImage(data.id);
                     }
                 });
-            }    
-        }            
+            }
+        }
         assignButtonEvent('.button-copy');
         assignButtonEvent('.button-edit');
         assignButtonEvent('.button-preview');
         assignButtonEvent('.button-delete');
-    }        
+    }
 
-    var imageDeleted = (json)  => {
-        rp.uploads.getUploadedImages();        
+    var imageDeleted = (json) => {
+        rp.uploads.getUploadedImages();
         let msg = `Associated image file [${json.name}] still available.`;
         notifier.show('Imaged deleted successfully from database', msg, '', '/assets/images/ok-48.png', 4000);
     }
 
-    var getImageListButtons =  () => {
+    var getImageListButtons = () => {
         let template = '<button title="{title}" class="button-{action}"><i class="fa fa-{icon}"></i></button>';
         let buttons = [];
-        buttons.push(template.replace('{action}','copy').
-                replace('{icon}','clipboard').
-                replace('{title}', 'Copy filename to clipboard'));
-        buttons.push(template.replace('{action}','edit').
-                replace('{icon}','pencil').
-                replace('{title}', 'Edit image'));
-        buttons.push(template.replace('{action}','preview').
-                replace('{icon}','picture-o').
-                replace('{title}', 'Preview image'));
-        buttons.push(template.replace('{action}','delete').
-                replace('{icon}','trash-o').
-                replace('{title}', 'Delete image'));                
+        buttons.push(template.replace('{action}', 'copy').replace('{icon}', 'clipboard').replace('{title}', 'Copy filename to clipboard'));
+        buttons.push(template.replace('{action}', 'edit').replace('{icon}', 'pencil').replace('{title}', 'Edit image'));
+        buttons.push(template.replace('{action}', 'preview').replace('{icon}', 'picture-o').replace('{title}', 'Preview image'));
+        buttons.push(template.replace('{action}', 'delete').replace('{icon}', 'trash-o').replace('{title}', 'Delete image'));
 
-        return buttons.join('');                
-    };        
+        return buttons.join('');
+    };
 
     var showImagesList = function(json) {
         let formatAsDateOnly = function(data, type) {
-            if (type=='display') {
+            if (type == 'display') {
                 return data.substr(0, 10);
-            }
-            else {
+            } else {
                 return data;
             }
-        };                
+        };
 
-        $('#datatable').DataTable().destroy();        
+        $('#datatable').DataTable().destroy();
         $('#datatable').DataTable({
             order: [2, 'desc'],
             data: json,
             columns: [
-                { data: 'name'},
-                { data: 'description', width: "25%"},
-                { data: 'updated_at', width: "15%", 
-                  render: function(data, type, row) {
-                      return formatAsDateOnly(data, type);
-                  }
+                { data: 'name' },
+                { data: 'description', width: "25%" },
+                {
+                    data: 'updated_at',
+                    width: "15%",
+                    render: function(data, type, row) {
+                        return formatAsDateOnly(data, type);
+                    }
                 },
                 { data: null, defaultContent: getImageListButtons(), width: "14%" }
-            ]                
-        });        
+            ]
+        });
     }
 
     return {
@@ -123,20 +113,20 @@ rp.uploads = (function() {
 })();
 
 
-rp.Lib.documentReady( function() {
+rp.Lib.documentReady(function() {
     // Assign button event handlers when datatable is drawn.
 
-    rp.viewImage.configureModalDialog();        
+    rp.viewImage.configureModalDialog();
     let editImageOptions = {
-        "elementIdTriggerAddNewImage" : "show-upload-window",
-        "elementIdWindowContents" : "upload-container",
-        "ajax" : false
+        "elementIdTriggerAddNewImage": "show-upload-window",
+        "elementIdWindowContents": "upload-container",
+        "ajax": false
     };
     rp.editImage.configureModalDialog(editImageOptions);
 
     let modalOptions = {
         "title": "This is the title",
-        "text":  "This is a description of the title"
+        "text": "This is a description of the title"
     };
     //rp.modalConfirm.configureModalDialog(modalOptions);
 
@@ -150,12 +140,10 @@ rp.Lib.documentReady( function() {
 
     document.getElementById('test-button').addEventListener('click', function(e) {
         let confirm = new rp.dialogs.modalConfirmation();
-        confirm.setContent(document.getElementById('modal-confirm-container').innerHTML);    
+        confirm.setContent(document.getElementById('modal-confirm-container').innerHTML);
         confirm.open(tester);
-        
+
     })
 
     rp.uploads.getUploadedImages();
 });
-
-
